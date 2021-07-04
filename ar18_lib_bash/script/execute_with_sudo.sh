6b38aacb501f73e8b6f3075050a57de1976e803a
nulysses <adr.zivil@gmail.com>
2021-07-04 17:05:10 +0200

    1

--- ar18_lib_bash/script/execute_with_sudo.sh
+++ ar18_lib_bash/script/execute_with_sudo.sh
@@ -24,9 +24,12 @@ function ar18.script._execute_with_sudo(){
     set -x
     IFS=' '
     local command
-    command="${@}"
-    echo "foo2"
-    echo "${ar18_sudo_password}" | eval "sudo -Sk ${@}"
+    local arg
+    for arg in "$@"; do
+      command="${command} \'${arg}\'"
+    done
+    echo "foo3"
+    echo "${ar18_sudo_password}" | eval "sudo -Sk ${command}"
     echo ""
     
     ###############################FUNCTION_END##################################
