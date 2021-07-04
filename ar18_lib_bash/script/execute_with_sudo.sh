656082f9f9717d1a49c1f94de0cf2886b9bf66b8
nulysses <adr.zivil@gmail.com>
2021-07-04 16:38:02 +0200

    1

--- ar18_lib_bash/script/execute_with_sudo.sh
+++ ar18_lib_bash/script/execute_with_sudo.sh
@@ -21,7 +21,7 @@ function ar18.script._execute_with_sudo(){
     
     ar18.script.import script.obtain_sudo_password
     ar18.script.obtain_sudo_password
-    
+    set -x
     IFS=' '
     local command
     command="'$*'"
