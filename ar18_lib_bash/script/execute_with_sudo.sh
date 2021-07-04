95033a751d62129e27a20201bc48435623c25e2c
nulysses <adr.zivil@gmail.com>
2021-07-04 17:35:52 +0200

    1

--- ar18_lib_bash/script/execute_with_sudo.sh
+++ ar18_lib_bash/script/execute_with_sudo.sh
@@ -6,22 +6,8 @@ function ar18.script._execute_with_sudo(){
 
 
   function ar18.script.execute_with_sudo() {
-    # Function template 2021-06-12.01
-    local LD_PRELOAD_old
-    LD_PRELOAD_old="${LD_PRELOAD}"
-    LD_PRELOAD=
-    local shell_options
-    IFS=$'\n' shell_options=($(shopt -op))
-    set -eu
-    set -o pipefail
-    local ret
-    ret=0
-    set +x
-    ##############################FUNCTION_START#################################
-    
     ar18.script.import script.obtain_sudo_password
     ar18.script.obtain_sudo_password
-    set -x
     IFS=' '
     local command
     command=""
@@ -29,17 +15,9 @@ function ar18.script._execute_with_sudo(){
     for arg in "$@"; do
       command="${command} \"${arg}\""
     done
-    echo "foo4"
+    echo ""
     echo "${ar18_sudo_password}" | eval "sudo -Sk ${command}"
     echo ""
-    
-    ###############################FUNCTION_END##################################
-    set +x
-    for option in "${shell_options[@]}"; do
-      eval "${option}"
-    done
-    LD_PRELOAD="${LD_PRELOAD_old}"
-    return "${ret}"
   }
   export -f "ar18.script.execute_with_sudo"
   
