08ea8201db79652ed90ab9311aa0dcc995f89539
nulysses <adr.zivil@gmail.com>
2021-07-07 08:24:31 +0200

    1

--- ar18_lib_bash/script/version_check.sh
+++ ar18_lib_bash/script/version_check.sh
@@ -22,6 +22,9 @@ function ar18.script._version_check(){
     }
     ##############################FUNCTION_START#################################
         
+    ar18.script.import ar18.script.obtain_sudo_password
+    ar18.script.import ar18.script.execute_with_sudo
+        
     ar18_version_checker_caller="$(caller | cut -d ' ' -f2-)"
     ar18_version_checker_caller="$(realpath "${ar18_version_checker_caller}")"
     ar18_version_checker_dir_name="$(dirname "${ar18_version_checker_caller}")"
@@ -35,6 +38,7 @@ function ar18.script._version_check(){
       echo "remote version is ${ar18_version_checker_module_version_remote}"
       if [[ "${ar18_version_checker_module_version_remote}" > "${ar18_version_checker_module_version_local}" ]]; then
         echo "new version available"
+        ar18.script.obtain_sudo_password
         if [ -f "/home/$(whoami)/.config/${ar18_version_checker_module_name}/INSTALL_DIR" ]; then
           echo "reinstalling"
         else
@@ -44,7 +48,7 @@ function ar18.script._version_check(){
           old_cwd="${PWD}"
           cd "/tmp/${ar18_version_checker_module_name}"
           git clone "http://github.com/ar18-linux/${ar18_version_checker_module_name}"
-          cp -raf "/tmp/${ar18_version_checker_module_name}/${ar18_version_checker_module_name}/${ar18_version_checker_module_name}/." "${ar18_version_checker_dir_name}/"
+          ar18.script.execute_with_sudo cp -raf "/tmp/${ar18_version_checker_module_name}/${ar18_version_checker_module_name}/${ar18_version_checker_module_name}/." "${ar18_version_checker_dir_name}/"
           cd "${old_cwd}"
           . "${ar18_version_checker_caller}"
         fi
