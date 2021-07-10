d914bfdc67b7e80b85de0ddc5aa51ebee59c730b
nulysses <adr.zivil@gmail.com>
2021-07-10 18:52:12 +0200

    1

--- ar18_lib_bash/script/version_check.sh
+++ ar18_lib_bash/script/version_check.sh
@@ -46,6 +46,7 @@ function ar18.script._version_check(){
         ar18.script.obtain_sudo_password
         if [ -f "/home/$(whoami)/.config/ar18/${ar18_version_checker_module_name}/INSTALL_DIR" ]; then
           echo "reinstalling"
+          echo "${ar18_version_checker_caller}"
           . "${ar18_version_checker_caller}"
         else
           echo "replacing"
