18497f6870ee3e71849745c478d3636d0e9c0098
nulysses <adr.zivil@gmail.com>
2021-07-11 00:02:47 +0200

    1

--- ar18_lib_bash/pip/install.sh
+++ ar18_lib_bash/pip/install.sh
@@ -8,7 +8,7 @@ function ar18.pip._install(){
   function ar18.pip.install() {
     # Prepare script environment
     {
-      # Function template version 2021-07-10_19:49:50
+      # Function template version 2021-07-11_00:01:52
       # Get old shell option values to restore later
       local shell_options
       shopt -s inherit_errexit
@@ -25,7 +25,7 @@ function ar18.pip._install(){
       ret=0
     }
     ##############################FUNCTION_START#################################
-                                            
+                                              
     local packages
     packages="$1"
     pip3 install ${packages}
