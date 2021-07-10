ebad1fd03bc300f4dabd5f70210c76e39d7f540f
nulysses <adr.zivil@gmail.com>
2021-07-10 19:51:28 +0200

    1

--- ar18_lib_bash/script/read_target.sh
+++ ar18_lib_bash/script/read_target.sh
@@ -8,7 +8,7 @@ function ar18.script._read_target(){
   function ar18.script.read_target() {
     # Prepare script environment
     {
-      # Function template version 2021-07-10_19:44:36
+      # Function template version 2021-07-10_19:49:50
       # Get old shell option values to restore later
       local shell_options
       shopt -s inherit_errexit
@@ -25,7 +25,7 @@ function ar18.script._read_target(){
       ret=0
     }
     ##############################FUNCTION_START#################################
-                                            
+                                              
     if [ ! -v ar18_deployment_target ]; then
       local ar18_deployment_target2
       set +u
