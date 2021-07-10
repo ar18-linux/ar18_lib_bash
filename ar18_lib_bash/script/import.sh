81f35654ef4cde2130ec0892bd9238f5287dd4f8
nulysses <adr.zivil@gmail.com>
2021-07-10 11:28:18 +0200

    1

--- ar18_lib_bash/script/import.sh
+++ ar18_lib_bash/script/import.sh
@@ -8,7 +8,7 @@ function ar18.script._import(){
   function ar18.script.import() {
     # Prepare script environment
     {
-      # Function template version 2021-07-09_20:12:35
+      # Function template version 2021-07-10_11:24:30
       # Get old shell option values to restore later
       local shell_options
       shopt -s inherit_errexit
@@ -25,7 +25,7 @@ function ar18.script._import(){
       ret=0
     }
     ##############################FUNCTION_START#################################
-              
+                  
     local to_import
     to_import="${1}"
     if [ ! -v import_map ]; then
--- ar18_lib_bash/script/obtain_sudo_password.sh
+++ ar18_lib_bash/script/obtain_sudo_password.sh
@@ -8,7 +8,7 @@ function ar18.script._obtain_sudo_password(){
   function ar18.script.obtain_sudo_password() {
     # Prepare script environment
     {
-      # Function template version 2021-07-09_20:12:35
+      # Function template version 2021-07-10_11:24:30
       # Get old shell option values to restore later
       local shell_options
       shopt -s inherit_errexit
@@ -25,7 +25,7 @@ function ar18.script._obtain_sudo_password(){
       ret=0
     }
     ##############################FUNCTION_START#################################
-                
+                    
     ar18.script.import script.has_sudo_capabilities
     
     local silent
