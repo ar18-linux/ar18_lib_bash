f9b2f2fe2092bfc4eb833c2c981b23eaddb7c4de
nulysses <adr.zivil@gmail.com>
2021-07-10 19:33:22 +0200

    1

--- ar18_lib_bash/script/import.sh
+++ ar18_lib_bash/script/import.sh
@@ -46,6 +46,7 @@ function ar18.script._import(){
         curl -O "https://raw.githubusercontent.com/ar18-linux/ar18_lib_bash/master/ar18_lib_bash/${to_import_transformed}.sh" > /dev/null 2>&1
         cd "${old_cwd}" 
         . "${target_path}"
+        echo "${import_map["${to_import}"]} imported"
       else
         target_path="$(cat "/home/$(whoami)/.config/ar18/ar18_lib_bash/INSTALL_DIR")/ar18_lib_bash/${to_import_transformed}.sh"
         . "${target_path}"
